cc4c09bd57c70cd94bdc591c7c4343f7a8a18302
EDY <edy@edy-linux.(none)>
2013-01-02 11:33:13 -0500

    fixes

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,4 @@
-# initializers/s3.rb
+
 if Rails.env == "production"
   # set credentials from ENV hash
   S3_CREDENTIALS = { 
@@ -6,3 +6,10 @@ if Rails.env == "production"
    	:secret_access_key => ENV['zxKj7aNDgWK04qPz7NDt8xIbVWxBff4bfsO9aX6O'],
     :bucket => "edymyecommerce"}
 end
+
+ #also need to set this vars to heroku
+ #heroku config:add AWS_ACCESS_KEY_ID='AKIAI3SFXWVKKD5YV3FQ'
+ #heroku config:add AWS_SECRET_ACCESS_KEY='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'
+ #heroku config:add AWS_BUCKET='edymyecommerce'
+ 
+ 
